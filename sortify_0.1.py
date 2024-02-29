f0138513082b9c19541b7def4d5e354da9c87332
Hamish C <47585050+hamishC0@users.noreply.github.com>
2024-02-29 23:24:01 +1000

    Added section to deal with files with only date information.

--- sortify_0.1.py
+++ sortify_0.1.py
@@ -15,10 +15,14 @@ import subprocess
 
 def update_metadata(file_path):
     """
-    Takes the file path as an input and searches it for a date in the forms YYYY-MM-DD HH mm ss,
-    YYYY MM DD HH mm ss, YYYY-MM-DD HH:mm:ss and YYYY-MM-DD. It takes this date string and puts it in datetime format then
-    applies the new datetime to the file metadata and prints a confirmation output. If a date cannnot be found 
-    in the filename it will print an error message and move on. 
+    Takes the file path as an input and searches it for a date in the 
+    forms: YYYY-MM-DD HH mm ss, YYYY MM DD HH mm ss, YYYY-MM-DD HH:mm:ss 
+    and YYYY-MM-DD (using 00:00:00 as default time values). 
+    
+    It takes this date string and puts it in datetime format then applies 
+    the new datetime to the file metadata and prints a confirmation output. 
+    If a date cannnot be found in the filename it will print an error message and move on. 
+    
     Parameters:
         file_path
     Returns: 
@@ -39,8 +43,20 @@ def update_metadata(file_path):
 
         else:
 
-            # print error message if date not found in file name
-            print(f"\033[91m'Error processing {filename}: Date not found in filename'\033[0m")
+            date_match = re.search(r'(\d{4}-\d{2}-\d{2})|(\d{4} \d{2} \d{2})', file_path)
+            if date_match:
+                creation_date_str = date_match.group(1)
+                creation_date = datetime.datetime.strptime(creation_date_str, '%Y-%m-%d')
+
+                # update creation date in file metadata using subprocess exiftool
+                subprocess.run(['exiftool', '-FileModifyDate=' + creation_date.strftime('%Y:%m:%d 00:00:00'), file_path])
+
+                # print confirmation of updated metadata
+                print(f"Updated metadata for {filename}")    
+
+            else :
+                # print error message if date not found in file name
+                print(f"\033[91m'Error processing {filename}: Date not found in filename'\033[0m")
 
     except Exception as e:
         # print error message for exceptions
